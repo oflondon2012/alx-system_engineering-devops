a2ded024e40311652d4bcbfb9cae6cc398a4f2c7
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-24 23:44:16 +0000

    puppet script to change config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 # puppet script to change my configuration file
 
-file { 'etc/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-
-	#client configuration
-	host*
-	#specify the file name as ~/0-RSA_public_key instead
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+     	# Client configuration
+	Host *
+	# Specify the file name as ~/0-RSA_public_key instead
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
-",
+  ",
 }
