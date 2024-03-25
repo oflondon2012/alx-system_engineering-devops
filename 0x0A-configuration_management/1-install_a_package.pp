b62f8e1ae482c8a75fa7bd1a36e10bf3ea156af4
oflondon2012 <gtadebayo2023@gmail.com>
2024-03-25 14:54:16 +0000

    task 1 and 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#script that install flask from 
-
-package { 'flask':
+#!/usr/bin/pup
+# scrit that install package 
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
