a471e830700b0516da05227d1538ec41542b507d
oflondon2012 <gtadebayo2023@gmail.com>
2024-04-01 14:11:07 +0000

    task 3

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,33 @@
+# Puppet to configure custom HTTP header response
+
+# Get the server update
+exec { 'update_server':
+    command => '/usr/bin/apt-get -y update',
+}
+
+#install nginx now
+package { 'ngix':
+   ensure => 'installed',
+}
+
+# ensure the service is running
+service { 'nginx':
+  ensure => 'running',
+  enable => 'true',
+  require => Package['nginx'],
+}
+
+# create hellow world in the server root
+file_world { '/var/www/html/index.html' :
+  content => 'Hello World!',
+}
+
+# customise response header for nginx
+
+file { 'Nginx header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+  require => Service['nginx'],
+}
