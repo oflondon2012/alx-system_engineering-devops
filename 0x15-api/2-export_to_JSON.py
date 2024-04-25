feb938acd75f18cb4a6156184282e7c8f07ea8f6
oflondon2012 <gtadebayo2023@gmail.com>
2024-04-25 09:23:20 +0000

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,25 +7,47 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
+def fetch_user_data(user_id):
+    """Fetches user data and TODO list for a given user ID."""
     url_str = 'https://jsonplaceholder.typicode.com/users/{}'
     todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    user = requests.get(url_str.format(user_id))
-    name = user.json().get('username')
-    todos = requests.get(todos_url)
-    todos_users = {}
-    tasks = []
-    for task in todos.json():
+
+    # Fetch user data
+    user_response = requests.get(url_str.format(user_id))
+    user_data = user_response.json()
+
+    # Fetch TODO list
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Filter tasks for the specific user
+    user_tasks = []
+    for task in todos_data:
         if task.get('userId') == int(user_id):
             task_dict = {
-                    "task": task.get('title'),
-                    "completed": task.get('completed'),
-                    "username": user.json().get('username')
-                    }
-            tasks.append(task_dict)
-        todos_users[user_id] = tasks
-    file = user_id + '.json'
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": user_data.get('username')
+            }
+            user_tasks.append(task_dict)
 
-    with open(file, mode="w") as user_file:
-        json.dump(todos_users, user_file)
+    return user_tasks
+
+
+def save_user_tasks(user_id):
+    """Saves user tasks to a JSON file."""
+    tasks = fetch_user_data(user_id)
+    user_data = {user_id: tasks}
+    file_name = f"{user_id}.json"
+
+    with open(file_name, mode="w") as user_file:
+        json.dump(user_data, user_file)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python module.py <user_id>")
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    save_user_tasks(user_id)
+
