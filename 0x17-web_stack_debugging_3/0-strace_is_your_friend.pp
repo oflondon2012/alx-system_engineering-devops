63c2fae4205b9d34bb05d7e62765eb012b1e4ac8
oflondon2012 <138879181+oflondon2012@users.noreply.github.com>
2024-06-06 01:04:25 +0100

    Delete 0x17-web_stack_debugging_3 directory
    Another former work

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# how to fix `phpp` extensions to `php` in the WordPress file `wp-settings.php`
-
-exec { 'wordpress-fix':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
